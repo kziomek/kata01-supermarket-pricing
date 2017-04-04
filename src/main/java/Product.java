0cfaa39392e4895cbbb685808df3bb4801f96a2b
Krzysztof Ziomek <kziomek@onet.eu>
2017-04-04 22:34:39 +0100

    Define Product

---
+++ src/main/java/Product.java
@@ -0,0 +1,15 @@
+import java.math.BigDecimal;
+
+/**
+ * @author Krzysztof Ziomek
+ * @since 04/04/2017.
+ */
+public class Product {
+
+    private String name;
+
+    private String barcode;
+
+    private BigDecimal price;
+
+}
