9fe57ad6e2563b15f0c23ef42a5691ef32a42252
Krzysztof Ziomek <kziomek@onet.eu>
2017-04-07 21:45:16 +0100

    Increase test coverage for ProductFactoryTest

---
+++ src/test/java/product/ProductFactoryTest.java
@@ -0,0 +1,49 @@
+package product;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static product.ProductEnum.BEANS;
+import static product.ProductEnum.COKE;
+import static product.ProductEnum.ORANGES;
+
+/**
+ * @author Krzysztof Ziomek
+ * @since 07/04/2017.
+ */
+public class ProductFactoryTest {
+
+    @Test
+    public void getProductShouldReturnBeansWhenBeansRequested(){
+
+        // execute
+        Product product = ProductFactory.getProduct(BEANS);
+
+        // assert
+        Assert.assertNotNull(product);
+        Assert.assertEquals(Beans.class, product.getClass());
+    }
+
+    @Test
+    public void getProductShouldReturnCokeWhenCokeRequested(){
+
+        // execute
+        Product product = ProductFactory.getProduct(COKE);
+
+        // assert
+        Assert.assertNotNull(product);
+        Assert.assertEquals(Coke.class, product.getClass());
+    }
+
+    @Test
+    public void getProductShouldReturnOrangesWhenOrangesRequested(){
+
+        // execute
+        Product product = ProductFactory.getProduct(ORANGES);
+
+        // assert
+        Assert.assertNotNull(product);
+        Assert.assertEquals(Oranges.class, product.getClass());
+    }
+
+}
